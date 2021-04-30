eae0dad908cf39e1698a6128d6bfd1298cc91cfd
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-04-30 18:17:51 +0100

    kotlin linting

--- src/app/src/main/kotlin/org/imperial/mrc/comet/controllers/MainController.kt
+++ src/app/src/main/kotlin/org/imperial/mrc/comet/controllers/MainController.kt
@@ -1,15 +1,11 @@
 package org.imperial.mrc.comet.controllers
 
-import net.logstash.logback.argument.StructuredArguments.kv
 import org.imperial.mrc.comet.clients.APIClient
-import org.imperial.mrc.comet.models.Response
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import kotlin.random.Random
 
 @RestController
 class MainController(

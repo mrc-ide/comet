852edb5d03c113c10d311c5f6ba37339826186fc
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-08-09 15:16:10 +0100

    Add playwright config and a simple test

---
+++ src/app/static/tests/e2e/home.etest.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("home page tests", () => {
+    test("renders homepage", async ({ page }) => {
+        await page.goto("/");
+        const name = await page.innerText("a.navbar-brand");
+        expect(name).toBe("comet");
+    });
+});

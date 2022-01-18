4b8ab71bb451b02f07243675bcf4875da9e4d01a
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-01-18 16:52:09 +0000

    Use common scripts to build app image

--- buildkite/push-app-image.sh
+++ buildkite/push-app-image.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-IMAGE_NAME=mrcide/comet
-
-docker build \
-  --build-arg BUILDKITE --build-arg BUILDKITE_BRANCH --build-arg BUILDKITE_BUILD_NUMBER --build-arg BUILDKITE_JOB_ID --build-arg BUILDKITE_BUILD_URL --build-arg BUILDKITE_PROJECT_SLUG --build-arg BUILDKITE_COMMIT --build-arg CODECOV_TOKEN \
-  --tag "$IMAGE_NAME:$GIT_BRANCH" --tag "$IMAGE_NAME:$GIT_COMMIT" \
-  --network=comet_nw \
-  "$HERE"/../src/
+HERE=$(realpath "$(dirname $0)")
+. $HERE/common
+. $HERE/build-app-image
 
 if [ "${BUILDKITE:-}" ]; then
   docker push "$IMAGE_NAME:$GIT_BRANCH"
--- buildkite/test-e2e.sh
+++ buildkite/test-e2e.sh
@@ -2,6 +2,7 @@
 set -ex
 HERE=$(dirname $0)
 . $HERE/common
+. $HERE/build-app-image
 
 function cleardocker() {
   $HERE/../scripts/clear-docker.sh
@@ -11,25 +12,18 @@ trap cleardocker EXIT
 # Run all dependencies
 $HERE/../scripts/run-dependencies.sh
 
-# TODO: shared script to buld app image, also called from push-image
 # Test against running app image
-  GIT_COMMIT=$(git rev-parse --short=7 HEAD)
-IMAGE_NAME=mrcide/comet
-docker build \
-  --build-arg BUILDKITE --build-arg BUILDKITE_BRANCH \
-  --build-arg BUILDKITE_BUILD_NUMBER --build-arg BUILDKITE_JOB_ID \
-  --build-arg BUILDKITE_BUILD_URL \
-  --build-arg BUILDKITE_PROJECT_SLUG \
-  --build-arg BUILDKITE_COMMIT --build-arg CODECOV_TOKEN \
-  --tag "$IMAGE_NAME:$GIT_BRANCH" --tag "$IMAGE_NAME:$GIT_COMMIT" \
-  --network=$NETWORK \
-  "$HERE"/../src/
-
 docker run -d \
     --network=$NETWORK \
     -p 8080:8080 \
     --name=comet \
     $IMAGE_NAME:$GIT_BRANCH
+
+# Create an image based on the shared build env that compiles and tests the front-end
+docker build --tag comet-test-e2e \
+	--build-arg GIT_ID=$GIT_ID \
+  -f $HERE/test-e2e.dockerfile \
+	.
 
 # Ensure that comet is available
 for _ in $(seq 30); do
@@ -38,13 +32,6 @@ for _ in $(seq 30); do
     fi
     sleep 6
 done
-
-
-# Create an image based on the shared build env that compiles and tests the front-end
-docker build --tag comet-test-e2e \
-	--build-arg GIT_ID=$GIT_ID \
-  -f $HERE/test-e2e.dockerfile \
-	.
 
 # Run the created image
 docker run --rm \
